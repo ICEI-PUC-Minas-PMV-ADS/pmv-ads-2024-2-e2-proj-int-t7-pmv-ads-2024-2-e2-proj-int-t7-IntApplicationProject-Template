bd8cee3be24d372400d93fa0a58258852b9492d6
Rafael Leles <68693480+rafaeleles@users.noreply.github.com>
2024-10-14 22:04:08 -0300

    Menu-Gestor-Cliente

--- projeto-concreta/src/app/pages/menu-gestor-cliente/menu-gestor-cliente.module.ts
+++ projeto-concreta/src/app/pages/menu-gestor-cliente/menu-gestor-cliente.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuGestorClienteComponent } from './menu-gestor-cliente.component';
 import { MenuGestorClienteRoutingModule } from './menu-gestor-cliente-routing.module';
-
-
+import { NavBarModule } from '../../navbar/navbar.module';
 
 @NgModule({
   declarations: [MenuGestorClienteComponent],
   imports: [
     CommonModule,
-    MenuGestorClienteRoutingModule
+    MenuGestorClienteRoutingModule,
+    NavBarModule
   ]
 })
 export class MenuGestorClienteModule { }
