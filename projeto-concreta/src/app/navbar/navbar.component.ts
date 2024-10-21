38100139b0b02fb1bdbf056cc3ee5424580806fa
Rafael Leles <68693480+rafaeleles@users.noreply.github.com>
2024-10-21 19:52:52 -0300

    Ajustes na navbar

--- projeto-concreta/src/app/navbar/navbar.component.ts
+++ projeto-concreta/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,37 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   isMenuOpen = false;
 
-  
-
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
+
+  constructor(
+    private router: Router,
+  ) {
+  }
+
+  scrollToSection(sectionId: string) {
+    if (sectionId === 'sobre') {
+      // Verifica se a página atual não é a home
+      if (this.router.url !== '/home') {
+        // Redireciona para a home
+        this.router.navigate(['/home']).then(() => {
+          // Aguarda a navegação antes de rolar
+          setTimeout(() => {
+            const section = document.getElementById(sectionId);
+            if (section) {
+              section.scrollIntoView({ behavior: 'smooth' });
+            }
+          }, 0); // Use um timeout para garantir que a rolagem ocorra após a navegação
+        });
+      } else {
+        // Se já estiver na home, apenas rola
+        const section = document.getElementById(sectionId);
+        if (section) {
+          section.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    }
+  }
+
 }
