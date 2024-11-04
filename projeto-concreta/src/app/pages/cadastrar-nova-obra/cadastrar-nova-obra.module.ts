eef11f3bdaf8308f604f4da8d2c75bc55b8a3349
02C4 <145403567+02C4@users.noreply.github.com>
2024-11-04 17:13:03 -0300

    cadastrar obra

--- projeto-concreta/src/app/pages/cadastrar-nova-obra/cadastrar-nova-obra.module.ts
+++ projeto-concreta/src/app/pages/cadastrar-nova-obra/cadastrar-nova-obra.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CadastrarNovaObraComponent } from './cadastrar-nova-obra.component';
 import { CadastrarNovaObraRoutingModule } from './cadastrar-nova-obra-routing.module';
+import { NavBarModule } from '../../navbar/navbar.module';
+import { FooterModule } from '../../footer/footer.module';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 
 
@@ -9,7 +14,12 @@ import { CadastrarNovaObraRoutingModule } from './cadastrar-nova-obra-routing.mo
   declarations: [CadastrarNovaObraComponent],
   imports: [
     CommonModule,
-    CadastrarNovaObraRoutingModule
+    CadastrarNovaObraRoutingModule,
+    NavBarModule,
+    FooterModule,
+    ButtonModule,
+    InputTextModule,
+    FloatLabelModule,
   ]
 })
 export class CadastrarNovaObraModule { }
