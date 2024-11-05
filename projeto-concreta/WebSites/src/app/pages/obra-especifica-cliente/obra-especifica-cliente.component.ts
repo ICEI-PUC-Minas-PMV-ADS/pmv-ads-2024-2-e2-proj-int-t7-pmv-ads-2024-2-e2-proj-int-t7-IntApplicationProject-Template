f4463bfbda7d40f87af88df705965cdfbb6e2755
outrasilva <jessica.1702@hotmail.com>
2024-11-05 12:56:57 -0300

    Estruturação das Telas de Obras Específicas .

---
+++ projeto-concreta/WebSites/src/app/pages/obra-especifica-cliente/obra-especifica-cliente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obra-especifica-cliente',
+  templateUrl: './obra-especifica-cliente.component.html',
+  styleUrl: './obra-especifica-cliente.component.scss'
+})
+export class ObraEspecificaClienteComponent {
+
+}
---
+++ projeto-concreta/WebSites/src/app/pages/obra-especifica-gestor/obra-especifica-gestor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obra-especifica-gestor',
+  templateUrl: './obra-especifica-gestor.component.html',
+  styleUrl: './obra-especifica-gestor.component.scss'
+})
+export class ObraEspecificaGestorComponent {
+
+}
