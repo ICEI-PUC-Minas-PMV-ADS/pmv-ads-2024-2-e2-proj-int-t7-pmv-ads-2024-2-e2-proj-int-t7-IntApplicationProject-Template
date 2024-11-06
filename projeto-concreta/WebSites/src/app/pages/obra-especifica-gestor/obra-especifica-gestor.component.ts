98ccadaa6d2825c2d05c7c7123cebc09905705e3
outrasilva <jessica.1702@hotmail.com>
2024-11-06 13:44:27 -0300

    Capa da obra editável.
    Adição do font-awesome para gerar o ícone de lápis.

--- projeto-concreta/WebSites/src/app/pages/obra-especifica-gestor/obra-especifica-gestor.component.ts
+++ projeto-concreta/WebSites/src/app/pages/obra-especifica-gestor/obra-especifica-gestor.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './obra-especifica-gestor.component.scss'
 })
 export class ObraEspecificaGestorComponent {
+  imageSrc: string = '../../../assets/capa-provisoria.svg'; //imagem padrão
 
+  editImage() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+
+    input.onchange = (event: Event) => {
+      const target = event.target as HTMLInputElement; // Verificação do tipo
+      if (target.files && target.files.length > 0) {
+        const file = target.files[0]; // Pega o primeiro arquivo
+        const reader = new FileReader();
+        
+        reader.onload = () => {
+          this.imageSrc = reader.result as string; // Atualiza a imagem
+        };
+        
+        reader.readAsDataURL(file); // Lê a imagem como URL
+      }
+    };
+
+    input.click(); 
+  }
 }
